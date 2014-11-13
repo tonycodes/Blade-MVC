d0e92c629a2f4ffac7487097aa1e74e770ee9a0a
Tony <anthony.james@gimo.co.uk>
2014-11-13 16:00:06 +0000

    Added comments and updated index.php

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
 
-require('/app/config.php');
-// Launcher (Bootstrap) file
-// Define routes here
+#########
+#
+# This is a small project im working on, if anyone finds it useful feel free to use and contribute to it.
+# Usage:
+# The idea is that we post slugs in the URL, grab them and then use this value to define a controller/function. 
+# This then allows you to simply invoke the blade templating engine very simply in the controller function, similar 
+# to Laravel in terms of templating. 
+#
+#########
 
-// Use slug from URI, point to relevant controller
-// Grab the slug
+# Grab the config file
+require('/app/config.php');
+
+# Grab the current route
 $slug = $_GET['slug'];
 
+# Insantiate the controller (this will be refactored)
 $HomeController = new HomeController();
 
+# Define some routes
 if ( $slug == 'home' ) { 
 	$HomeController->home(); 
 }
+
+# If all else fails, cut loose!
 else {
 	header('404');
 }
